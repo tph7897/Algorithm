d70043add979f9fed943d38c25da0bfbb90f5d62
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-11-06 13:13:19 +0900

    Update 완주하지 못한 선수.js

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,6 +1,8 @@
 function solution(participant, completion) {
+// 주어진 선수들의 이름을 정렬
     participant.sort();
-    completion.sort();
+    completion.sort(); 
+// 정렬된 선수들의 이름을 순차적으로 비교하여 맞지 않는 이름 출력
   for (let i = 0; i < participant.length; i++) {
     if (participant[i] !== completion[i]) {
       return participant[i];
