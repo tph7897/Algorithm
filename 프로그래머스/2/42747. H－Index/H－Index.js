d674145eda78ab172f0d86d8abdede034410455a
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-11-09 14:29:14 +0900

    [level 2] Title: H-Index, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,13 +1,23 @@
 function solution(citations) {
-    let answer = 0
-    let count = 0
-    citations.sort((a,b)=>b-a)
-    for(let i= 0; i<citations.length; i++){
-    count++
-        if(citations[i]<count){
-            break
+    // 반환할 H-Index 값을 저장하는 변수
+    let answer = 0;
+    // 현재까지 확인한 논문의 수를 나타내는 변수
+    let count = 0;
+    // 논문의 인용 횟수를 내림차순으로 정렬
+    citations.sort((a, b) => b - a);
+    // 모든 논문을 확인하면서 H-Index 계산
+    for (let i = 0; i < citations.length; i++) {
+        // 현재까지 확인한 논문의 수를 증가
+        count++;
+        // 현재 논문의 인용 횟수가 현재까지 확인한 논문의 수보다 작으면 반복 종료
+        if (citations[i] < count) {
+            count--
+            break;
+            
         }
-        answer++
+        // // H-Index 값을 증가
+        // answer++;
     }
-    return answer;
+    // 최종적으로 계산된 H-Index 반환
+    return count;
 }
