b22c31e300f0181fbd855838d24fe93bdfacd1a5
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-11-10 15:08:39 +0900

    [level 1] Title: 최소직사각형, Time: 4.56 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,10 +1,6 @@
 function solution(sizes) {
     let cardW = []
     let cardH = []
-    sizes.forEach((size) => (cardW.push(Math.max(...size)), cardH.push(Math.min(...size))))             
-    // sizes.forEach((size) =>{
-    //     cardW.push(Math.max(...size))
-    //     cardH.push(Math.min(...size))
-    //              })
+    sizes.forEach((size) => (cardW.push(Math.max(...size)), cardH.push(Math.min(...size))))   
     return Math.max(...cardW)*Math.max(...cardH)
 }
