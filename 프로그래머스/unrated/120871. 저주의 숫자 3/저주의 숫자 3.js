64169c2b14d9a778b3eba34a0662fae059a81390
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-11-29 22:52:41 +0900

    [level unrated] Title: 저주의 숫자 3, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    for(let i = 1 ; i <= n ; i++){
+        answer++
+        while(answer%3==0 || String(answer).includes('3')){
+            answer++
+        }
+    }
+    return answer;
+}
