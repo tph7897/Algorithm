63805b7ddc2bda8e26ef6d3314986c6231f15bdd
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-11-29 18:00:39 +0900

    [level unrated] Title: 유한소수 판별하기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,13 +1,13 @@
 function solution(a, b) {
     let answer = 1
-    for(let i = b ; i > 1 ; i--){
+    for(let i = b ; i > 1 ; i--){ // 기약분수 만들기
         if(a%i == 0 && b%i == 0){
             a /= i
             b /= i
             break
         }
     }
-    for(let i = 2 ; i <= b ; i++){
+    for(let i = 2 ; i <= b ; i++){ // 분모의 소인수 판별하여 2 또는 5가 아닐시에 2로 반환
         if(b%i == 0){
             b /= i
             if(i !== 2 && i !== 5){
