0b7292e2345d12537e5d81218eae374076103651
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-12-03 20:08:49 +0900

    [level unrated] Title: 캐릭터의 좌표, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -0,0 +1,24 @@
+function solution(keyinput, board) {
+    let location = [0,0]
+    for(let i = 0 ; i < keyinput.length ; i++){
+        switch(keyinput[i]) {
+          case 'up':
+            if(location[1]<(board[1]-1)/2){
+                location[1] += 1}
+            break
+          case 'down':
+            if(location[1]> -(board[1]-1)/2){
+                location[1] -= 1}
+            break
+          case 'right':
+            if(location[0]<(board[0]-1)/2){
+                location[0] += 1}
+            break
+          case 'left':
+            if(location[0]> -(board[0]-1)/2){
+                location[0] -= 1}
+            break
+        }
+    }
+    return location;
+}
