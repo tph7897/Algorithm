a56e164d8da866b8ae247cc14c76c754420673ed
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-12-04 10:56:51 +0900

    [level unrated] Title: 겹치는 선분의 길이, Time: 0.13 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -0,0 +1,11 @@
+function solution(lines) {
+    let answer = 0;
+        for(let i =- 100 ; i < 100 ; i++){
+            let cnt = 0;
+            if(lines[0][0]<=i && i<lines[0][1])cnt++;
+            if(lines[1][0]<=i && i<lines[1][1])cnt++;
+            if(lines[2][0]<=i && i<lines[2][1])cnt++;
+            if(cnt>1)answer++;
+        }
+    return answer
+}
