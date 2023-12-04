196500f2fb0f05ee0a0bb918afe9ec4d991ae24e
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-12-04 10:57:28 +0900

    [level unrated] Title: 겹치는 선분의 길이, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,6 +1,6 @@
 function solution(lines) {
     let answer = 0;
-        for(let i =- 100 ; i < 100 ; i++){
+        for(let i = -100 ; i < 100 ; i++){
             let cnt = 0;
             if(lines[0][0]<=i && i<lines[0][1])cnt++;
             if(lines[1][0]<=i && i<lines[1][1])cnt++;
