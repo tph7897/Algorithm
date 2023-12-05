2fde0476ada7bd282e93d97129f8c8ef76a1a173
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-12-05 11:23:18 +0900

    [level unrated] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/unrated/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,7 +1,3 @@
 function solution(strlist) {
-    var answer = [];
-    for(let i = 0 ; i < strlist.length ; i++){
-        answer.push(strlist[i].length)
-    }
-    return answer;
+    return strlist.map(str => str.length);
 }
