89c60fa42b046b5d3f9a4434f4d6ce73f16f9b5f
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-12-06 14:02:52 +0900

    [level unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = 0;
+    if(n % 2){
+        for(let i = 1 ; i <= n ; i+=2){
+            answer += i
+        }
+    }else{
+        for(let i = 2 ; i <= n ; i+=2){
+            answer += i**2
+        }
+    }
+    return answer;
+}
