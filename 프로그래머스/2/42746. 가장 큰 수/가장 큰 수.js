0bda10b8aac3ec9c3ac961773f2c755d934f1b32
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-12-07 09:21:14 +0900

    [level 2] Title: 가장 큰 수, Time: 0.10 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -2,5 +2,5 @@ function solution(numbers) {
     const answer = numbers.map(num => num.toString())
                      .sort((a, b) => (b + a) - (a + b))
                      .join('');
-    return answer[0] == "0" ? "0":answer
+    return answer[0] == "0" ? "0" : answer
 }
