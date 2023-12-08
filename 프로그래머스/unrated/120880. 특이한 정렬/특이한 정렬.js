df81e4d88c5423aa5f3beda5c69598838dcc46cc
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-12-08 11:14:10 +0900

    [level unrated] Title: 특이한 정렬, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/unrated/120880. 특이한 정렬/특이한 정렬.js
@@ -1,6 +1,4 @@
 function solution(numlist, n) {
-    let answer = numlist.map(i => i-n)
-                    .sort((a,b)=>a+b==0?b-a:Math.abs(a)-Math.abs(b))
-                    .map(i => i+n)
+    let answer = numlist.sort((a,b)=>(a-n)+(b-n)==0?(b-n)-(a-n):Math.abs(a-n)-Math.abs(b-n))           
     return answer;
 }
