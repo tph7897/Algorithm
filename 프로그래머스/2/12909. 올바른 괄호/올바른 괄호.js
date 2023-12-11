c66d4aedc3b57352dd779abbabfba19c4a5109d9
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-12-11 23:52:05 +0900

    [level 2] Title: 올바른 괄호, Time: 2.45 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,12 +1,8 @@
 function solution(s){
     var answer = 0;
-    if(s.length%2 == 1){
-         return false
-    }else{
         for(let i = 0 ; i < s.length ; i++){
             s[i] == '(' ? answer++ : answer--
             if(answer < 0) break
         }
-        return answer? false : true 
-    }
+    return answer ? false : true 
 }
