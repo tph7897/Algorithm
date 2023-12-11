cfdaf6a300f8789496e7d1d6862c3e47b5007f4d
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-12-11 23:32:23 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,21 @@
+function solution(progresses, speeds) {
+    var answer = [];
+    let finish = 0;
+    let count = 0;
+    progresses = progresses.map((e, i) => Math.ceil((100 - e) / speeds[i]));
+    while (finish < progresses.length) {
+        progresses = progresses.map((e) => --e);
+        while (progresses[finish] <= 0) {
+            count++;
+            finish++;
+            if (finish >= progresses.length) {
+                break;
+            }
+        }
+        if (count > 0) {
+            answer.push(count);
+            count = 0;
+        }
+    }
+    return answer;
+}
