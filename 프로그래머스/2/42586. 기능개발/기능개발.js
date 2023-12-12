fd1d25dd1796d523e99acee6474144d4eaaabc8e
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-12-12 09:47:17 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,14 +1,20 @@
 function solution(progresses, speeds) {
-    let answer = [0];
-    let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
-    let maxDay = days[0];
-
-    for(let i = 0, j = 0; i< days.length; i++){
-        if(days[i] <= maxDay) {
-            answer[j] += 1;
-        } else {
-            maxDay = days[i];
-            answer[++j] = 1;
+    var answer = [];
+    let finish = 0;
+    let count = 0;
+    progresses = progresses.map((e, i) => Math.ceil((100 - e) / speeds[i]));
+    while (finish < progresses.length) {
+        progresses = progresses.map((e) => --e);
+        while (progresses[finish] <= 0) {
+            count++;
+            finish++;
+            if (finish >= progresses.length) {
+                break;
+            }
+        }
+        if (count > 0) {
+            answer.push(count);
+            count = 0;
         }
     }
     return answer;
