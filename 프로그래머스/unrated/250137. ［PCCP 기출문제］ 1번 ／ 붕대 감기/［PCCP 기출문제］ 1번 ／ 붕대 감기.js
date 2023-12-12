c6ffa151c123560d901eda922399d3f77ba9a268
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-12-12 21:13:50 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -0,0 +1,30 @@
+function solution(bandage, health, attacks) {
+    let HP = health
+    var answer = 0;
+    let attack = 0;
+    let heelTime = -1;
+    let atTime = attacks.map(i => i[0])
+    let atDmg = attacks.map(i => i[1])
+    for(let i = 0 ; i <= attacks[attacks.length-1][0] ; i++){ // 시간지남
+        
+        if(i==atTime[attack]){ // 몬스터 공격
+            HP -= atDmg[attack]
+            if(HP < 1) break
+            attack++
+            heelTime = 0
+        }else{ // 공격하지 않을때
+            HP += bandage[1]
+            if(HP > health){ // 최대  체력을 넘을 수 없음
+                HP = health
+            }
+            heelTime++
+        }
+
+        if(heelTime >= bandage[0]){ // 붕대 다 감았을때 추가 회복
+            heelTime = 0
+            HP += bandage[2]
+        }
+        // console.log([i, HP, heelTime, order])
+    }
+    return HP > 0 ? HP : -1 ;
+}
