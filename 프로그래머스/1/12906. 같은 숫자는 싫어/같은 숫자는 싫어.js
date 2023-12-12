d3c18bcab092b886b6574d75c816e8a724c1142f
tph7897 <132332533+tph7897@users.noreply.github.com>
2023-12-12 09:31:09 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 48.53 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,10 +1,3 @@
-function solution(arr)
-{
-    var answer = [];
-    for(let i = 0; i<arr.length ; i++){
-        if(arr[i]!==arr[i+1]) {
-            answer.push(arr[i])
-        } 
-    }
-    return answer;
+function solution(arr){
+    return arr.filter((e, i)=> e !== arr[i+1]);
 }
