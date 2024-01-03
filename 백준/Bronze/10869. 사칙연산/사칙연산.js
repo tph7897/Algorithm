8271ba731305a6397b234b22525dac60751614ca
tph7897 <132332533+tph7897@users.noreply.github.com>
2024-01-04 00:07:59 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,8 @@
+var input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+const A = +input[0]
+const B = +input[1]
+console.log(A+B);
+console.log(A-B);
+console.log(A*B);
+console.log(Math.floor(A/B));
+console.log(A%B);
