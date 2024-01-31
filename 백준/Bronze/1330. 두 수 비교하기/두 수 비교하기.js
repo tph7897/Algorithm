55518f2c82880844ff966a67c2b337d437baf4fb
E_namgyu <132332533+tph7897@users.noreply.github.com>
2024-01-31 17:34:37 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -0,0 +1,6 @@
+var input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+if(a==b) console.log("==")
+if(a>b) console.log(">")
+else if(a<b) console.log("<")
