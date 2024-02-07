655fa101a39e3abdbb10e6a17e445ec92dae4431
E_namgyu <132332533+tph7897@users.noreply.github.com>
2024-02-07 16:46:55 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,13 +1,4 @@
 function solution(arr) {
-    const n = arr.length
-    var answer = 1;
-    for(let i = 0 ; i < n ; i++ ){
-        for(let j = 0 ; j < n ; j++ ){
-            if(arr[i][j] !== arr[j][i]){
-                answer = 0
-                break
-            }
-        }
-    }
+    var answer = 0;
     return answer;
 }
