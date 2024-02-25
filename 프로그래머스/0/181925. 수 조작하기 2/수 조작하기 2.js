621452409997daba54289dcfb931b72ae385def1
E_namgyu <132332533+tph7897@users.noreply.github.com>
2024-02-26 08:21:59 +0900

    [level 0] Title: 수 조작하기 2, Time: 7.18 ms, Memory: 41.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,10 @@
+function solution(numLog) {
+    var answer = ''
+    for(let i = 1 ; i < numLog.length ; i++){
+        if(numLog[i]-numLog[i-1] == 1) answer += "w"
+        else if(numLog[i]-numLog[i-1] == -1) answer += "s"
+        else if(numLog[i]-numLog[i-1] == 10) answer += "d"
+        else answer += "a"
+    }
+    return answer;
+}
