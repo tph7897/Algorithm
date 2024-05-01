6f039e0cfd537fa83336d9735fbb2c81c60a1b96
E_namgyu <132332533+tph7897@users.noreply.github.com>
2024-05-02 03:23:25 +0900

    [Bronze III] Title: 공 넣기, Time: 140 ms, Memory: 9372 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.js
@@ -0,0 +1,13 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+const [N, M] = input[0].split(' ').map(Number)
+const baskets = new Array(N).fill(0)
+
+for (let i =1 ; i<=M;i++){
+    const [start,end, ball] = input[i].split(' ').map(Number)
+    
+    for(let j = start ; j <=end;j++){
+        baskets[j-1] = ball
+    }
+}
+
+console.log(baskets.join(" "))
