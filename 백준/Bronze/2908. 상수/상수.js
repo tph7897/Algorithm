126b1828fa544407b7aaf171ec55545b40402b9b
E_namgyu <132332533+tph7897@users.noreply.github.com>
2024-05-16 08:57:25 +0900

    [Bronze II] Title: 상수, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.js
@@ -0,0 +1,6 @@
+const input = require("fs").readFileSync("dev/stdin").toString().trim().split(" ");
+
+const a = input[0].split("").reverse().join("");
+const b = input[1].split("").reverse().join("");
+
+console.log(Math.max(a, b));
