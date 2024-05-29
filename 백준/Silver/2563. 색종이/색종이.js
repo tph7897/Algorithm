2d43b0a6960641148ea7ca05f541ccb1e68edf36
E_namgyu <132332533+tph7897@users.noreply.github.com>
2024-05-30 08:46:16 +0900

    [Silver V] Title: 색종이, Time: 180 ms, Memory: 9960 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.js
@@ -0,0 +1,21 @@
+const lines = require('fs').readFileSync(`dev/stdin`).toString().trim().split(`\n`);
+
+let drawingPaper = Array.from(Array(100), () => Array(100).fill(0));
+const confetti = lines.slice(1).map(line => line.split(` `).map(Number));
+let size = 0;
+
+if (Number(lines[0]) !== confetti.length) return;
+
+confetti.forEach(line => {
+    const [x, y] = line;
+
+    for(let i = x; i < x+10; i++) {
+        for(let j = y; j < y+10; j++) {
+            if(1 === drawingPaper[i][j]) continue;
+            drawingPaper[i][j] = 1;
+            size++;
+        }
+    }
+})
+
+console.log(size)
