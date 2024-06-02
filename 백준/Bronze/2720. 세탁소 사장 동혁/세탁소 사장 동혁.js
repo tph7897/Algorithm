dc8f9753b616bb9e519899d6ac011f7245fff10d
E_namgyu <132332533+tph7897@users.noreply.github.com>
2024-06-03 07:12:08 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 120 ms, Memory: 9940 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.js
@@ -0,0 +1,15 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+
+for (let i = 1; i < input.length; i++) {
+  console.log(solution(input[i]));
+}
+
+function solution(change) {
+  let count = [0, 0, 0, 0];
+  const coins = [25, 10, 5, 1];
+  for (let i = 0; i < coins.length; i++) {
+    count[i] += Math.floor(change / coins[i]);
+    change %= coins[i];
+  }
+  return count.join(' ');
+}
