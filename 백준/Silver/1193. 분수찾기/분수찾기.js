158d24364eabc4f419110c1f15283b092040d2e3
E_namgyu <132332533+tph7897@users.noreply.github.com>
2024-06-06 08:44:34 +0900

    [Silver V] Title: 분수찾기, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.js
@@ -0,0 +1,17 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+let number = Number(input);
+let value = 1;
+while (true) {
+	number -= value;
+	if (number <= 0) {
+		number += value;
+		break;
+	}
+	value++;
+}
+
+if (value % 2 === 1) {
+	console.log(`${value - (number - 1)}/${1 + (number - 1)}`);
+} else {
+	console.log(`${1 + (number - 1)}/${value - (number - 1)}`);
+}
