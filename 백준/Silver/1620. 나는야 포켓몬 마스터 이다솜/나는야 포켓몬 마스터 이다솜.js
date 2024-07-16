6037daad73eb801218f16c1bb450277b70b49502
E_namgyu <132332533+tph7897@users.noreply.github.com>
2024-07-16 12:01:52 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 340 ms, Memory: 59168 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.js
@@ -0,0 +1,18 @@
+const input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+const [N, M] = input.shift().split(' ').map(v=>+v);
+const NumToName = new Map();
+const NameToNum = new Map();
+for (let i = 0; i < N; i++) {
+    NumToName.set(i+1, input[i]);
+    NameToNum.set(input[i], i+1);
+}
+const quiz = input.slice(N, input.length);
+let answer = '';
+quiz.forEach(v=> {
+    if (isNaN(v)) {
+        answer += NameToNum.get(v) + '\n';
+    } else {
+        answer += NumToName.get(+v) + '\n';
+    }
+});
+console.log(answer.trim());
