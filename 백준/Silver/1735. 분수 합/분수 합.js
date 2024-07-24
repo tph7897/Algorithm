8048ffa341564499df33ed291bbae013bbfaafb7
E_namgyu <132332533+tph7897@users.noreply.github.com>
2024-07-24 19:17:52 +0900

    [Silver III] Title: 분수 합, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.js
@@ -0,0 +1,16 @@
+const [moles, denoms] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let [fM, fD] = moles.split(" ").map(BigInt);
+let [sM, sD] = denoms.split(" ").map(BigInt);
+
+let mole = fM * sD + sM * fD;
+let denom = fD * sD;
+
+const gcd = (x, y) => {
+  if (y == 0) return x;
+  else return gcd(y, x % y);
+};
+
+const divisor = gcd(mole, denom);
+
+console.log(`${mole / divisor} ${denom / divisor}`);
