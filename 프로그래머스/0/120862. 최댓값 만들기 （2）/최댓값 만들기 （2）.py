0f4670698cd1fa9f9468233286ad147d65ce9880
E_namgyu <132332533+tph7897@users.noreply.github.com>
2024-11-30 19:22:24 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,8 +1,3 @@
 def solution(n):
-    
     n.sort();
-    num1 = n[0] * n[1];
-    num2 = n[len(n) - 1] * n[len(n) - 2];
-    num3 = n[0] * n[len(n) - 1];
-    
-    return max(num1, num2, num3)
+    return max(n[0] * n[1], n[- 1] * n[- 2])
